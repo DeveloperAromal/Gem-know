f23a1519bff07e060c70ba041696439274fd0fe7
DeveloperAromal <153208245+DeveloperAromal@users.noreply.github.com>
2024-11-01 07:37:49 +0530

    hamburger updated

--- app/components/includes/CustomHeader.tsx
+++ app/components/includes/CustomHeader.tsx
@@ -3,7 +3,12 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookF, faTwitter, faYoutube, faVimeo } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebookF,
+  faTwitter,
+  faYoutube,
+  faVimeo,
+} from "@fortawesome/free-brands-svg-icons";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 export default function CustomHeader() {
@@ -19,18 +24,36 @@ export default function CustomHeader() {
       <header className="w-full h-10 bg-teal-700 flex justify-end items-center">
         <div className="flex gap-4 pb-1">
           <div className="flex items-center space-x-2 text-white">
-            <span className="text-xs md:text-sm cursor-pointer">Engage ATS</span>
+            <span className="text-xs md:text-sm cursor-pointer hover:text-yellow-400">
+              Engage ATS
+            </span>
             <span>|</span>
-            <span className="text-xs md:text-sm cursor-pointer">Canvas</span>
+            <span className="text-xs md:text-sm cursor-pointer hover:text-yellow-400">Canvas</span>
             <span>|</span>
-            <span className="text-xs md:text-sm cursor-pointer">Contact Us</span>
+            <span className="text-xs md:text-sm cursor-pointer hover:text-yellow-400">
+              Contact Us
+            </span>
           </div>
           <div className="flex items-center space-x-4 text-white">
-            <FontAwesomeIcon icon={faFacebookF} className="cursor-pointer hover:text-sky-500" />
-            <FontAwesomeIcon icon={faTwitter} className="cursor-pointer hover:text-sky-500" />
-            <FontAwesomeIcon icon={faVimeo} className="cursor-pointer hover:text-pink-500" />
-            <FontAwesomeIcon icon={faYoutube} className="cursor-pointer hover:text-red-500" />
-            <span className="cursor-pointer text-xs md:text-sm hidden lg:block md:block ">ATS Home Page</span>
+            <FontAwesomeIcon
+              icon={faFacebookF}
+              className="cursor-pointer hover:text-sky-500"
+            />
+            <FontAwesomeIcon
+              icon={faTwitter}
+              className="cursor-pointer hover:text-sky-500"
+            />
+            <FontAwesomeIcon
+              icon={faVimeo}
+              className="cursor-pointer hover:text-pink-500"
+            />
+            <FontAwesomeIcon
+              icon={faYoutube}
+              className="cursor-pointer hover:text-red-500"
+            />
+            <span className="cursor-pointer text-xs md:text-sm hidden lg:block md:block ">
+              ATS Home Page
+            </span>
           </div>
         </div>
         <div className="h-full w-32 md:w-40 text-center py-2 bg-yellow-400 hover:bg-teal-800 text-white ml-4 md:ml-10 hidden lg:block md:block">
@@ -45,7 +68,7 @@ export default function CustomHeader() {
             <motion.img
               src="/logo.png"
               alt="Logo"
-              className="h-10 md:h-16 rounded-xl" // Increased logo size for smaller screens
+              className="h-24 md:h-20 rounded-xl" // Increased logo size for both smaller and larger screens
               initial={{ opacity: 0, x: -50 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.5 }}
@@ -53,13 +76,27 @@ export default function CustomHeader() {
           </div>
 
           <div className="md:hidden">
-            <button onClick={toggleMenu} aria-label="Toggle menu" className="text-teal-700">
+            <button
+              onClick={toggleMenu}
+              aria-label="Toggle menu"
+              className="text-teal-700 relative z-20"
+            >
               <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} size="lg" />
             </button>
           </div>
 
-          <ul className={`flex-col gap-2 absolute md:static top-20 left-0 w-full md:w-auto md:flex md:flex-row bg-white transition-transform duration-300 ${isMenuOpen ? "flex" : "hidden"} text-teal-700 font-semibold`}>
-            {["Who We Are", "Membership", "Accreditation", "Resources", "Events"].map((item, index) => (
+          <ul
+            className={`flex-col gap-2 absolute md:static top-8 left-0 w-full md:w-auto md:flex md:flex-row bg-white transition-transform duration-300 py-20 ${
+              isMenuOpen ? "flex" : "hidden"
+            } text-teal-700 font-semibold`}
+          >
+            {[
+              "Who We Are",
+              "Membership",
+              "Accreditation",
+              "Resources",
+              "Events",
+            ].map((item, index) => (
               <motion.li
                 key={index}
                 className="cursor-pointer hover:text-yellow-500 p-2 md:p-0 text-center text-sm md:text-base" // Adjusted text size for responsiveness
