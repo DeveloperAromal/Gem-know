7d3c1deceb9de988ec946441c34874d86c7cb843
DeveloperAromal <153208245+DeveloperAromal@users.noreply.github.com>
2024-12-12 14:52:13 +0530

    added admission form

--- app/academics/admission-enrollment/page.tsx
+++ app/academics/admission-enrollment/page.tsx
@@ -1,5 +1,6 @@
 import CustomHeader from "@/app/components/includes/CustomHeader";
 import { Metadata } from "next";
+import Footer from "@/app/components/screens/Footer";
 
 export const metadata: Metadata = {
   title: "Online Application - Gem Know Model HSS",
@@ -20,11 +21,14 @@ export default function AdmissionEnrollment() {
           </p>
         </div>
         <form className="max-w-4xl mx-auto p-8 space-y-6">
-         
           <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 gap-6">
             <div>
-              <label htmlFor="firstName" className="block text-gray-700 font-medium">
-                First Name of the child <span  className="text-xl text-red-600">*</span>
+              <label
+                htmlFor="firstName"
+                className="block text-gray-700 font-medium"
+              >
+                First Name of the child{" "}
+                <span className="text-xl text-red-600">*</span>
               </label>
               <input
                 type="text"
@@ -35,8 +39,11 @@ export default function AdmissionEnrollment() {
               />
             </div>
             <div>
-              <label htmlFor="lastName" className="block text-gray-700 font-medium">
-                Surname <span  className="text-xl text-red-600">*</span>
+              <label
+                htmlFor="lastName"
+                className="block text-gray-700 font-medium"
+              >
+                Surname <span className="text-xl text-red-600">*</span>
               </label>
               <input
                 type="text"
@@ -49,17 +56,19 @@ export default function AdmissionEnrollment() {
           </div>
           <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 gap-6">
             <div>
-              <label htmlFor="standard" className="block text-gray-700 font-medium">
-                Standard to which admission is sought <span  className="text-xl text-red-600">*</span>
+              <label
+                htmlFor="standard"
+                className="block text-gray-700 font-medium"
+              >
+                Standard to which admission is sought{" "}
+                <span className="text-xl text-red-600">*</span>
               </label>
               <select
                 id="standard"
                 className="w-full border border-teal-300 rounded-md p-2 focus:ring-2 focus:ring-teal-600"
                 required
               >
-                <option value="">
-                  Select Standard
-                </option>
+                <option value="">Select Standard</option>
                 <option value="UKG">UKG</option>
                 <option value="LKG">LKG</option>
                 <option value="1">1</option>
@@ -78,7 +87,7 @@ export default function AdmissionEnrollment() {
             </div>
             <div>
               <label htmlFor="dob" className="block text-gray-700 font-medium">
-                Date of Birth <span  className="text-xl text-red-600">*</span>
+                Date of Birth <span className="text-xl text-red-600">*</span>
               </label>
               <input
                 type="date"
@@ -90,26 +99,38 @@ export default function AdmissionEnrollment() {
           </div>
 
           <div>
-            <label className="block text-gray-700 font-medium">Gender <span  className="text-xl text-red-600">*</span></label>
+            <label className="block text-gray-700 font-medium">
+              Gender <span className="text-xl text-red-600">*</span>
+            </label>
             <div className="flex items-center gap-4">
               <div>
                 <input type="radio" id="male" name="gender" value="Male" />
-                <label htmlFor="male" className="ml-2 text-gray-600">Male</label>
+                <label htmlFor="male" className="ml-2 text-gray-600">
+                  Male
+                </label>
               </div>
               <div>
                 <input type="radio" id="female" name="gender" value="Female" />
-                <label htmlFor="female" className="ml-2 text-gray-600">Female</label>
+                <label htmlFor="female" className="ml-2 text-gray-600">
+                  Female
+                </label>
               </div>
               <div>
                 <input type="radio" id="other" name="gender" value="Other" />
-                <label htmlFor="other" className="ml-2 text-gray-600">Other</label>
+                <label htmlFor="other" className="ml-2 text-gray-600">
+                  Other
+                </label>
               </div>
             </div>
           </div>
 
           <div>
-            <label htmlFor="address" className="block text-gray-700 font-medium">
-              Residential Address <span  className="text-xl text-red-600">*</span>
+            <label
+              htmlFor="address"
+              className="block text-gray-700 font-medium"
+            >
+              Residential Address{" "}
+              <span className="text-xl text-red-600">*</span>
             </label>
             <textarea
               id="address"
@@ -121,8 +142,12 @@ export default function AdmissionEnrollment() {
 
           <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 gap-6">
             <div>
-              <label htmlFor="fatherName" className="block text-gray-700 font-medium">
-                Father&apos;s Name <span  className="text-xl text-red-600">*</span>
+              <label
+                htmlFor="fatherName"
+                className="block text-gray-700 font-medium"
+              >
+                Father&apos;s Name{" "}
+                <span className="text-xl text-red-600">*</span>
               </label>
               <input
                 type="text"
@@ -133,8 +158,12 @@ export default function AdmissionEnrollment() {
               />
             </div>
             <div>
-              <label htmlFor="fatherPhone" className="block text-gray-700 font-medium">
-                Father&apos;s Phone No <span  className="text-xl text-red-600">*</span>
+              <label
+                htmlFor="fatherPhone"
+                className="block text-gray-700 font-medium"
+              >
+                Father&apos;s Phone No{" "}
+                <span className="text-xl text-red-600">*</span>
               </label>
               <input
                 type="tel"
@@ -148,8 +177,12 @@ export default function AdmissionEnrollment() {
 
           <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 gap-6">
             <div>
-              <label htmlFor="motherName" className="block text-gray-700 font-medium">
-                Mother&apos;s Name <span  className="text-xl text-red-600">*</span>
+              <label
+                htmlFor="motherName"
+                className="block text-gray-700 font-medium"
+              >
+                Mother&apos;s Name{" "}
+                <span className="text-xl text-red-600">*</span>
               </label>
               <input
                 type="text"
@@ -160,8 +193,12 @@ export default function AdmissionEnrollment() {
               />
             </div>
             <div>
-              <label htmlFor="motherPhone" className="block text-gray-700 font-medium">
-                Mother&apos;s Phone No <span  className="text-xl text-red-600">*</span>
+              <label
+                htmlFor="motherPhone"
+                className="block text-gray-700 font-medium"
+              >
+                Mother&apos;s Phone No{" "}
+                <span className="text-xl text-red-600">*</span>
               </label>
               <input
                 type="tel"
@@ -175,8 +212,11 @@ export default function AdmissionEnrollment() {
 
           <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 gap-6">
             <div>
-              <label htmlFor="nationality" className="block text-gray-700 font-medium">
-                Nationality <span  className="text-xl text-red-600">*</span>
+              <label
+                htmlFor="nationality"
+                className="block text-gray-700 font-medium"
+              >
+                Nationality <span className="text-xl text-red-600">*</span>
               </label>
               <input
                 type="text"
@@ -187,8 +227,11 @@ export default function AdmissionEnrollment() {
               />
             </div>
             <div>
-              <label htmlFor="motherTongue" className="block text-gray-700 font-medium">
-                Mother Tongue <span  className="text-xl text-red-600">*</span>
+              <label
+                htmlFor="motherTongue"
+                className="block text-gray-700 font-medium"
+              >
+                Mother Tongue <span className="text-xl text-red-600">*</span>
               </label>
               <input
                 type="text"
@@ -201,31 +244,35 @@ export default function AdmissionEnrollment() {
           </div>
 
           <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 gap-6">
-          <div>
-          <label htmlFor="religion" className="block text-gray-700 font-medium">
-              Religion <span  className="text-xl text-red-600">*</span>
-            </label>
-            <input
-              type="text"
-              id="religion"
-              className="w-full border border-teal-300 rounded-md p-2 focus:ring-2 focus:ring-teal-600"
-              placeholder="Enter Religion"
-              required
-            />
-            
-          </div>
-          <div>
-          <label htmlFor="standard" className="block text-gray-700 font-medium">
-                Standard to which admission is sought <span  className="text-xl text-red-600">*</span>
+            <div>
+              <label
+                htmlFor="religion"
+                className="block text-gray-700 font-medium"
+              >
+                Religion <span className="text-xl text-red-600">*</span>
+              </label>
+              <input
+                type="text"
+                id="religion"
+                className="w-full border border-teal-300 rounded-md p-2 focus:ring-2 focus:ring-teal-600"
+                placeholder="Enter Religion"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="standard"
+                className="block text-gray-700 font-medium"
+              >
+                Standard to which admission is sought{" "}
+                <span className="text-xl text-red-600">*</span>
               </label>
               <select
                 id="standard"
                 className="w-full border border-teal-300 rounded-md p-2 focus:ring-2 focus:ring-teal-600"
                 required
               >
-                <option value="">
-                  Select Category
-                </option>
+                <option value="">Select Category</option>
                 <option value="General">General</option>
                 <option value="OBC">OBC</option>
                 <option value="SC">SC</option>
@@ -233,38 +280,43 @@ export default function AdmissionEnrollment() {
                 <option value="EWS">EWS</option>
                 <option value="Other">Other</option>
               </select>
+            </div>
           </div>
-          </div>
-
 
           <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 gap-6">
-          <div>
-          <label htmlFor="Previous School( if any )" className="block text-gray-700 font-medium">
-              Previous School ( if any ) <span  className="text-xl text-red-600">*</span>
-            </label>
-            <input
-              type="text"
-              id="School"
-              className="w-full border border-teal-300 rounded-md p-2 focus:ring-2 focus:ring-teal-600"
-              placeholder="Enter School"
-              required
-            />
-            
-          </div>
-          <div>
-          <label htmlFor="Grade in which last studied" className="block text-gray-700 font-medium">
-                Grade in which last studied <span  className="text-xl text-red-600">*</span>
+            <div>
+              <label
+                htmlFor="Previous School( if any )"
+                className="block text-gray-700 font-medium"
+              >
+                Previous School ( if any ){" "}
+                <span className="text-xl text-red-600">*</span>
               </label>
               <input
-              type="text"
-              id="Grade"
-              className="w-full border border-teal-300 rounded-md p-2 focus:ring-2 focus:ring-teal-600"
-              placeholder="Enter Religion"
-              required
-            />
+                type="text"
+                id="School"
+                className="w-full border border-teal-300 rounded-md p-2 focus:ring-2 focus:ring-teal-600"
+                placeholder="Enter School"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="Grade in which last studied"
+                className="block text-gray-700 font-medium"
+              >
+                Grade in which last studied{" "}
+                <span className="text-xl text-red-600">*</span>
+              </label>
+              <input
+                type="text"
+                id="Grade"
+                className="w-full border border-teal-300 rounded-md p-2 focus:ring-2 focus:ring-teal-600"
+                placeholder="Enter Religion"
+                required
+              />
+            </div>
           </div>
-          </div>
-
 
           <div className="text-center">
             <button
@@ -276,6 +328,7 @@ export default function AdmissionEnrollment() {
           </div>
         </form>
       </section>
+      <Footer />
     </section>
   );
 }
