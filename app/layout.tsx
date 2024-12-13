4d6e86a56b7a7454b249ea87105e2704f8f0846c
DeveloperAromal <153208245+DeveloperAromal@users.noreply.github.com>
2024-12-13 07:37:59 +0530

    changed image formate

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/logo-round.png",
+        url: "/logo-roundco.avif",
         width: 1200,
         height: 630,
         alt: "Gem Know Model HSS",
